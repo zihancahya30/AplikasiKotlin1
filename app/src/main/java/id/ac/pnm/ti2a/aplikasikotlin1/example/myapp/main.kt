70db1c6db43b40c4782695103fdf0f17b84216c1
zihancahya30 <160403673+zihancahya30@users.noreply.github.com>
2024-03-17 22:14:45 +0700

    Add files via upload

---
+++ app/src/main/java/id/ac/pnm/ti2a/aplikasikotlin1/example/myapp/main.kt
@@ -0,0 +1,25 @@
+package id.ac.pnm.ti2a.aplikasikotlin1.example.myapp
+
+import java.lang.Math.PI
+
+fun buildAquarium() {
+    val myAquarium = Aquarium(width = 25, length = 25, height = 40)
+    myAquarium.printSize()
+    val myTower = TowerTank(diameter = 25, height = 40)
+    myTower.printSize()
+}
+
+fun makeFish() {
+    val shark = Shark()
+    val pleco = Plecostomus()
+    println("Shark: ${shark.color}")
+    shark.eat()
+    println("Plecostomus: ${pleco.color}")
+    pleco.eat()
+}
+
+fun main (){
+    buildAquarium()
+    makeFish()
+}
+
